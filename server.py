48cf40c994b4f3f5240e9e5f16b099eaf8404430
Joe Torreggiani <joetorreggiani@gmail.com>
2022-02-12 23:00:01 -0500

    Change application code.

--- server.py
+++ server.py
@@ -4,13 +4,11 @@ import socketserver
 
 from http import HTTPStatus
 
-# Test comment
-
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello there!! You requested %s' % (self.path)
+        msg = 'You requirest the path: %s' % (self.path)
         self.wfile.write(msg.encode())
 
 port = int(os.getenv('PORT', 80))
